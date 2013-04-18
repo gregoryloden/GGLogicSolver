d2d51e0252cf7849866df6cbaee6c3ffc977a6ee
Gail Terman <gterman@gmail.com>
2013-04-17 20:43:33 -0400

    commented

--- AlloyCompiler.java
+++ AlloyCompiler.java
@@ -10,20 +10,31 @@ import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
 import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
 
+/**
+ * AlloyCompiler reads a file given as an argument, solves it, and places the output in out.xml.
+ */
 public class AlloyCompiler {
   public static void main (String[] args) throws Err{
+    // Get the file from the argument list
     String filename = args[0];
+    // parse the model of the world
     Module mod = CompUtil.parseEverything_fromFile(null, null, filename);
 
+    // set basic options for Alloy solver
     A4Options opts = new A4Options();
     opts.solver = A4Options.SatSolver.SAT4J;
 
+    // get the current working directory
     String cwd = System.getProperty("user.dir");
 
+    // solve the model
     for (Command command: mod.getAllCommands()) {
       A4Solution ans = TranslateAlloyToKodkod.execute_command(null, mod.getAllReachableSigs(), command, opts);
+      // if the model has a solution
       if (ans.satisfiable()) {
+        // write it to the file
         ans.writeXML(cwd+"/out.xml");
+       // otherwise print an inscrutable error that will make our code crash in horriffic ways
       }else System.out.println("Not satisfiable.");
     }
   }
--- plaintext_solver.py
+++ plaintext_solver.py
@@ -4,13 +4,16 @@ from solve import solve
 import sys
 
 def parse_file(filename):
+  """ Parse a file written in plaintext logic language format that is horrifyingly undocumented and specific."""
   f = open(filename, 'r')
   header = f.readline()
+  # parse the header
   tokens = header.split()
   num_cat = int(tokens[0])
   num_mem = int(tokens[1])
   ordered = bool(tokens[2])
   puzzle = Puzzle()
+  # get the members, add them to the puzzle, and to the helper.
   helper = {}
   for x in range(num_cat):
     cat = Category(f.readline().strip(), num_mem, ordered)
@@ -21,8 +24,11 @@ def parse_file(filename):
       cat.add(mem)
       helper[mem_name] = mem
     puzzle.categories.append(cat)
+  # use reflection to read the rest of the lines as clues!
+  # TODO fix it so it doesn't crash if there are empty lines at the end of the file
   for line in f.readlines():
     tokens = line.split()
+    # special rules for Or. Hacky.
     if len(tokens) == 7 and tokens[3] == "Or":
       m1 = helper[tokens[0]]
       m2 = helper[tokens[2]]
@@ -36,8 +42,11 @@ def parse_file(filename):
       m2 = helper[tokens[2]]
       fun_string = tokens[1] + "(m1, m2)"
     puzzle.relationships.append(eval(fun_string))
+  # return the puzzle object
   return puzzle
 
 if __name__ == "__main__":
+  # if called from the command line also solve the puzzle
+  # intended for informed debugging only; may break in startling ways
   solve(parse_file(sys.argv[1]))
 
